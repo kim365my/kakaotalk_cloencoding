34d93fc6a7632d01a645aa225913a4e7a0a6adb8
ringingm <102598905+ringingm@users.noreply.github.com>
2022-10-26 04:13:22 +0900

    오류 수정

--- script/script.js
+++ script/script.js
@@ -1,5 +1,8 @@
 const audio = new Audio("./music/짱구브금.mp3");
+const playAudio = document.querySelector(".fa-music");
 
-audio.onclick = function() {
+function onAudio() {
   audio.play();
 }
+playAudio.addEventListener("click", onAudio);
+
